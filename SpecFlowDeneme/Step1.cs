c6203c69dae5cadfe94c08c520cb8dbd869ca3fe
Çelik Gümüşdağ <celikgumusdag@gmail.com>
2016-06-15 09:51:00 +0300

    v 1

---
+++ SpecFlowDeneme/Step1.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TechTalk.SpecFlow;
+
+namespace SpecFlowDeneme
+{
+    [Binding]
+    public class CalculatorSteps
+    {
+        private int result { get; set; }
+        private Calculator cal = new Calculator();
+
+
+        [Given(@"I have entered (.*) into the calculator")]
+        public void GivenIHaveEnteredİntoTheCalculator(int p0)
+        {
+            Console.WriteLine("Given");
+            cal.FirstNumber = p0;
+        }
+        
+        [Given(@"I have also entered (.*) into the calculator")]
+        public void GivenIHaveAlsoEnteredİntoTheCalculator(int p0)
+        {
+            cal.SecondNumber = p0;
+        }
+       
+
+        [When(@"I press add")]
+        public void WhenIPressAdd()
+        {
+            result = cal.Add();
+        }
+        
+        [Then(@"the result should be (.*) on the screen")]
+        public void ThenTheResultShouldBeOnTheScreen(int p0)
+        {
+            Assert.AreEqual(p0, result);
+        }
+
+    }
+}
